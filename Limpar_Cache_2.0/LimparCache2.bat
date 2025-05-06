22ac6d0a151b9d65810b76feaf0904696375cd84
VitorSouza01 <souza.programing@gmail.com>
2025-05-05 23:58:28 -0300

    third commit

--- Limpar_Cache_2.0/LimparCache2.bat
+++ Limpar_Cache_2.0/LimparCache2.bat
@@ -3,21 +3,79 @@
 chcp 65001 >nul
 title Limpar Cache
 
-echo ====================================================================================================
+: MENU
+cls
+echo =====================================================================================================
 echo(
-echo ##       #### ##     ## ########     ###    ########   ######     ###     ######  ##     ## ######## 
-echo ##        ##  ###   ### ##     ##   ## ##   ##     ## ##    ##   ## ##   ##    ## ##     ## ##       
-echo ##        ##  #### #### ##     ##  ##   ##  ##     ## ##        ##   ##  ##       ##     ## ##       
-echo ##        ##  ## ### ## ########  ##     ## ########  ##       ##     ## ##       ######### ######   
-echo ##        ##  ##     ## ##        ######### ##   ##   ##       ######### ##       ##     ## ##       
-echo ##        ##  ##     ## ##        ##     ## ##    ##  ##    ## ##     ## ##    ## ##     ## ##       
-echo ######## #### ##     ## ##        ##     ## ##     ##  ######  ##     ##  ######  ##     ## ######## 
+echo  ##       #### ##     ## ########     ###    ########   ######     ###     ######  ##     ## ######## 
+echo  ##        ##  ###   ### ##     ##   ## ##   ##     ## ##    ##   ## ##   ##    ## ##     ## ##       
+echo  ##        ##  #### #### ##     ##  ##   ##  ##     ## ##        ##   ##  ##       ##     ## ##       
+echo  ##        ##  ## ### ## ########  ##     ## ########  ##       ##     ## ##       ######### ######   
+echo  ##        ##  ##     ## ##        ######### ##   ##   ##       ######### ##       ##     ## ##       
+echo  ##        ##  ##     ## ##        ##     ## ##    ##  ##    ## ##     ## ##    ## ##     ## ##       
+echo  ######## #### ##     ## ##        ##     ## ##     ##  ######  ##     ##  ######  ##     ## ######## 
 echo(
-echo ====================================================================================================
-echo( -
-echo - Selecione a opção que deseja limpar o cache do Windows
+echo =====================================================================================================
+echo(
+echo  [ DIGITE O NÚMERO CORRESPONDENTE À OPÇÃO DESEJADA PARA LIMPAR O CACHE DO WINDOWS ]
+echo(
+
+echo  [1] - Limpeza Usuário.
+echo  ^> Limpa arquivos temporários do usuário.
+echo(
+echo  [2] - Limpeza Sistema.
+echo ^> Limpa arquivos temporários do sistema.
+echo(
+echo  [3] - Limpeza DNS.
+echo ^> Limpa cache do DNS.
+echo(
+echo  [4] - Limpeza Completa.
+echo  ^> Limpa todos os caches.
+echo(
+set /p opcao= Digite o número da opção desejada:
+
+if "%opcao%"=="1" (
+	:: Limpa arquivos temporários do usuário
+	echo impando arquivos temporários do usuário...
+	del /q /f /s "%TEMP%\*.*"
+	rd /s /q "%TEMP%"
+	pause
+	goto MENU
+	
+) else if "%opcao%"=="2" (
+	:: Limpa arquivos temporários do sistema
+	echo Limpando arquivos temporários do sistema...
+	del /q /f /s "C:\Windows\Temp\*.*"
+	rd /s /q "C:\Windows\Temp"
+	pause
+	goto MENU
+	
+) else if "%opcao%"=="3" (
+	:: Limpa cache do DNS
+	echo Limpando cache de DNS...
+	ipconfig /flushdns
+	pause
+	goto MENU
+	
+) else if "%opcao%"=="4" (
+	:: Limpa todo os cache
+	echo Limpando todos os caches...
+	del /q /f /s "%TEMP%\*.*"
+	rd /s /q "%TEMP%"
+	pause
+	del /q /f /s "C:\Windows\Temp\*.*"
+	rd /s /q "C:\Windows\Temp"
+	pause
+	ipconfig /flushdns
+	pause
+	goto MENU
+
+) else (
+	echo(
+	echo [ OPÇÃO INVÁLIDA! TENTE NOVAMENTE! ]
+	echo(
+	pause
+	goto MENU
+)
 
 pause > nul
-
-
-
